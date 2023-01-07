7b7dc6c3a892aab6eb02cfd1fd42fa76f9bb4fa2
epicminix <119180434+epicminix@users.noreply.github.com>
2023-01-07 14:04:57 +0900

    Lol
    Lol

---
+++ main.py
@@ -0,0 +1,13 @@
+﻿import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    content = message.content
+    author = message.author
+
+    if content.startswith('!ping'):
+        await message.channel.send("{0.author.mention}: pong!".format(message))
+
+client.run('YOUR_TOKEN_HERE', bot=False) 
